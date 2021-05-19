e16ca539fc0a6b1b21e674d18a8d0c0228d381a2
FD <fedorova2.do@edu.spbstu.ru>
2021-05-19 21:00:45 +0300

    siple test with workspace

--- test/org.jkiss.dbeaver.swtbottest/src/org/jkiss/dbeaver/swtbottest/framework/MainWindow.java
+++ test/org.jkiss.dbeaver.swtbottest/src/org/jkiss/dbeaver/swtbottest/framework/MainWindow.java
@@ -1,6 +1,10 @@
 package org.jkiss.dbeaver.swtbottest.framework;
 
+import org.eclipse.swtbot.swt.finder.SWTBot;
 import org.eclipse.swtbot.swt.finder.waits.Conditions;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotButton;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+
 import static org.jkiss.dbeaver.swtbottest.framework.DBeaver.bot;
 import static org.jkiss.dbeaver.swtbottest.framework.Constants.NEW_DATABASE_CONNECTION;
 import static org.jkiss.dbeaver.swtbottest.framework.Constants.CONNECT_TO_DATABASE;
@@ -8,7 +12,10 @@ import static org.jkiss.dbeaver.swtbottest.framework.Constants.CONNECT_TO_DATABA
 public class MainWindow{
 
     public void openConnectionDialog(){
-        bot.toolbarButton(NEW_DATABASE_CONNECTION).click();
-//        bot.waitUntil(Conditions.shellIsActive(CONNECT_TO_DATABASE));
+    	bot.tree().getTreeItem("DBeaver Sample Database (SQLite)").expand();
+    	bot.tree().getTreeItem("DBeaver Sample Database (SQLite)").getNode("Таблицы").expand();
+    	bot.tree().getTreeItem("DBeaver Sample Database (SQLite)").getNode("Таблицы").getNode("Album").select();
+    	bot.tree().getTreeItem("DBeaver Sample Database (SQLite)").getNode("Таблицы").getNode("Album").doubleClick();
+    	bot.editorByTitle("Album").show();
     }
 }
--- test/org.jkiss.dbeaver.swtbottest/src/org/jkiss/dbeaver/swtbottest/tests/ConnectionDialogTest.java
+++ test/org.jkiss.dbeaver.swtbottest/src/org/jkiss/dbeaver/swtbottest/tests/ConnectionDialogTest.java
@@ -1,11 +1,12 @@
 package  org.jkiss.dbeaver.swtbottest.tests;
 
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.junit.Test;
 
 public class ConnectionDialogTest extends BaseTest {
 
     @Test
     public void connectionDialogHasAllDB(){
-    		dBeaver.mainWindow.openConnectionDialog();
+       dBeaver.mainWindow.openConnectionDialog();
     }
 }

2d48a5ee879f40dce60f536780ca5a2c56149100
FD <fedorova2.do@edu.spbstu.ru>
2021-05-20 08:31:45 +0300

    Now it works with clean workspace

--- test/org.jkiss.dbeaver.swtbottest/src/org/jkiss/dbeaver/swtbottest/framework/MainWindow.java
+++ test/org.jkiss.dbeaver.swtbottest/src/org/jkiss/dbeaver/swtbottest/framework/MainWindow.java
@@ -1,6 +1,8 @@
 package org.jkiss.dbeaver.swtbottest.framework;
 
 import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.swt.finder.keyboard.Keystrokes;
+import org.eclipse.swtbot.swt.finder.utils.SWTBotPreferences;
 import org.eclipse.swtbot.swt.finder.waits.Conditions;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotButton;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
@@ -12,10 +14,9 @@ import static org.jkiss.dbeaver.swtbottest.framework.Constants.CONNECT_TO_DATABA
 public class MainWindow{
 
     public void openConnectionDialog(){
-    	bot.tree().getTreeItem("DBeaver Sample Database (SQLite)").expand();
-    	bot.tree().getTreeItem("DBeaver Sample Database (SQLite)").getNode("Таблицы").expand();
-    	bot.tree().getTreeItem("DBeaver Sample Database (SQLite)").getNode("Таблицы").getNode("Album").select();
-    	bot.tree().getTreeItem("DBeaver Sample Database (SQLite)").getNode("Таблицы").getNode("Album").doubleClick();
-    	bot.editorByTitle("Album").show();
+    	SWTBotShell shell = bot.activeShell();
+    	SWTBotPreferences.KEYBOARD_LAYOUT = "EN_US";
+    	shell.pressShortcut(Keystrokes.LF);
+    	bot.sleep(5000);
     }
 }
